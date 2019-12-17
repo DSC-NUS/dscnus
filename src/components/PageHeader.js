c91acc260ade6221fc57b0b1ec30ebb308f3e80f
TSY <example@mail.com>
2019-12-17 14:52:21 +0800

    Added header and home page

---
+++ src/components/PageHeader.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Menu, Dropdown, Segment, Responsive, Button, Icon, Sidebar, Header, Image } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import styles from '../styles/Header.module.css';
+
+class PageHeader extends Component {
+    render() {
+        return (
+            <div>
+                Header
+            </div>
+    )
+  }
+}
+
+export default PageHeader;

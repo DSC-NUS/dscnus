5804c461214a198d494fb4c5d4644e1e78a8b332
TSY <example@mail.com>
2020-01-04 11:22:23 +0800

    Updates

---
+++ precache-manifest.aaa9e01ac1f23ffe718ff0333a61ce7d.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df0e4846156169bc35ebaa07649a0333",
+    "url": "/DSC-NUS/dscnus/index.html"
+  },
+  {
+    "revision": "dd947203054d7589136e",
+    "url": "/DSC-NUS/dscnus/static/css/2.0474670c.chunk.css"
+  },
+  {
+    "revision": "bde45a1f09f427689806",
+    "url": "/DSC-NUS/dscnus/static/css/main.f2432e72.chunk.css"
+  },
+  {
+    "revision": "dd947203054d7589136e",
+    "url": "/DSC-NUS/dscnus/static/js/2.7656757f.chunk.js"
+  },
+  {
+    "revision": "21d42a4bf3460b0ba68550daf47cbd29",
+    "url": "/DSC-NUS/dscnus/static/js/2.7656757f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bde45a1f09f427689806",
+    "url": "/DSC-NUS/dscnus/static/js/main.47945220.chunk.js"
+  },
+  {
+    "revision": "9139589d0335755b173a",
+    "url": "/DSC-NUS/dscnus/static/js/runtime-main.719b47fd.js"
+  },
+  {
+    "revision": "e702191f00e2cfa8a9b5ce1e1210162c",
+    "url": "/DSC-NUS/dscnus/static/media/A.e702191f.jpg"
+  },
+  {
+    "revision": "a45c803801fbbe8c3be4e0d34033dea4",
+    "url": "/DSC-NUS/dscnus/static/media/ANN_3566.a45c8038.jpg"
+  },
+  {
+    "revision": "bb7d71c3e8cddbe385ed16c93af28e26",
+    "url": "/DSC-NUS/dscnus/static/media/B.bb7d71c3.jpg"
+  },
+  {
+    "revision": "822e04092da84fa23043f590c1d1b58a",
+    "url": "/DSC-NUS/dscnus/static/media/C.822e0409.jpg"
+  },
+  {
+    "revision": "631fbfdf4f5eb3e24f70faab588f4ace",
+    "url": "/DSC-NUS/dscnus/static/media/Finance.631fbfdf.jpg"
+  },
+  {
+    "revision": "527ae81dfe7be640d1281430d975a71d",
+    "url": "/DSC-NUS/dscnus/static/media/Operations.527ae81d.JPG"
+  },
+  {
+    "revision": "1b9ffa306a059a2a3fde789ec6548137",
+    "url": "/DSC-NUS/dscnus/static/media/Partnership.1b9ffa30.jpg"
+  },
+  {
+    "revision": "e508987f686bbd800e564f663020b4fa",
+    "url": "/DSC-NUS/dscnus/static/media/People.e508987f.jpg"
+  },
+  {
+    "revision": "e71472229f4bd094d902b2da5226e8ae",
+    "url": "/DSC-NUS/dscnus/static/media/Pubs.e7147222.JPG"
+  },
+  {
+    "revision": "b917feb57663f07c78cfaa1965e0b2e4",
+    "url": "/DSC-NUS/dscnus/static/media/Technology.b917feb5.jpg"
+  },
+  {
+    "revision": "8247bdfe5fa472ec44f694918daa4b6c",
+    "url": "/DSC-NUS/dscnus/static/media/aboutuspagebg.8247bdfe.jpg"
+  },
+  {
+    "revision": "bdcf9343c81e47e0197adf66ab1fc109",
+    "url": "/DSC-NUS/dscnus/static/media/blogbg.bdcf9343.jpg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/DSC-NUS/dscnus/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/DSC-NUS/dscnus/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/DSC-NUS/dscnus/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/DSC-NUS/dscnus/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/DSC-NUS/dscnus/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "c7a9456dbd9113b56a7fc55c6e0efc57",
+    "url": "/DSC-NUS/dscnus/static/media/contactbg.c7a9456d.jpg"
+  },
+  {
+    "revision": "94345bfdb61a78cbf7b95caef07cc4cb",
+    "url": "/DSC-NUS/dscnus/static/media/different.94345bfd.jpg"
+  },
+  {
+    "revision": "745383fa7391a3e992dc304b0aa8e877",
+    "url": "/DSC-NUS/dscnus/static/media/event1.745383fa.jpg"
+  },
+  {
+    "revision": "64bfa6e521194cd9ec14119e0d472f73",
+    "url": "/DSC-NUS/dscnus/static/media/event2.64bfa6e5.jpg"
+  },
+  {
+    "revision": "94345bfdb61a78cbf7b95caef07cc4cb",
+    "url": "/DSC-NUS/dscnus/static/media/event3.94345bfd.jpg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/DSC-NUS/dscnus/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "bae8c21a2b9443a1e8767cce2558c89a",
+    "url": "/DSC-NUS/dscnus/static/media/header.bae8c21a.jpg"
+  },
+  {
+    "revision": "0ab12442ce84f04a605533fef80d3ea3",
+    "url": "/DSC-NUS/dscnus/static/media/homepage.0ab12442.jpg"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/DSC-NUS/dscnus/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/DSC-NUS/dscnus/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/DSC-NUS/dscnus/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/DSC-NUS/dscnus/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/DSC-NUS/dscnus/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/DSC-NUS/dscnus/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/DSC-NUS/dscnus/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/DSC-NUS/dscnus/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/DSC-NUS/dscnus/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/DSC-NUS/dscnus/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "33fd72c66157fe722c59d3e78963800b",
+    "url": "/DSC-NUS/dscnus/static/media/project1.33fd72c6.png"
+  },
+  {
+    "revision": "19ca977ab0086941a2aaf1c994cd2c21",
+    "url": "/DSC-NUS/dscnus/static/media/whatwedo.19ca977a.jpg"
+  },
+  {
+    "revision": "6a5230ee566f9c88931a7a9a8666a09f",
+    "url": "/DSC-NUS/dscnus/static/media/whoarewe.6a5230ee.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dscnus/precache-manifest.b535d2f1623a93f07466895377e25f27.js"
+  "/DSC-NUS/dscnus/precache-manifest.aaa9e01ac1f23ffe718ff0333a61ce7d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dscnus/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/DSC-NUS/dscnus/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

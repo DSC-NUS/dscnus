d8883c65246718f8c3b7fe9854344e8bd3b3133c
aliciatxl <example@mail.com>
2020-03-04 20:36:57 +0800

    Change ga code

--- src/data/googleanalytics.js
+++ src/data/googleanalytics.js
@@ -1,7 +1,7 @@
 import ReactGA from 'react-ga';
 import auth from './auth.ts'; // Sample authentication provider
 
-const trackingId = "UA-147601557-3"; // Replace with your Google Analytics tracking ID
+const trackingId = "UA-147601557-4"; // Replace with your Google Analytics tracking ID
 ReactGA.initialize(trackingId);
 ReactGA.set({
   userId: auth.currentUserId(),

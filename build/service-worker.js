d28b550a0fcc3e89785ba4307d2ea96bebc7ddc7
aliciatxl <example@mail.com>
2020-06-03 15:55:47 +0800

    run build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.87feac1d734652aa568591be74948fff.js"
+  "/precache-manifest.38b60c9735423b248157719b381be19e.js"
 );
 
 self.addEventListener('message', (event) => {
